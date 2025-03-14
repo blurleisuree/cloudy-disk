d08c6812382d7af52486445170e8e809fab2f7f7
blurleisure <ipishir@gmail.com>
2025-03-14 13:31:27 +0300

    Fixed CORS policy for production

--- server/middleware/cors.middleware.js
+++ server/middleware/cors.middleware.js
@@ -1,7 +1,19 @@
 const cors = require("cors");
 
+const allowedOrigins = [
+  "https://cloudy-disk-client.onrender.com",
+  "http://localhost:3000",
+];
+
 const corsMiddleware = cors({
-  origin: "http://localhost:3000",
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
 });
 
