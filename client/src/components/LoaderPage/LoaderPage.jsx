cae85102a55353064f390183b1cf93896995e122
blurleisure <ipishir@gmail.com>
2025-03-31 21:29:11 +0300

    replaced Loader to shared folder

--- client/src/components/LoaderPage/LoaderPage.jsx
+++
@@ -1,9 +0,0 @@
-function LoaderPage() {
-  return (
-    <div className="w-screen h-screen flex items-center justify-center">
-      <div className="w-16 h-16 border-4 border-t-primary-color border-gray-300 rounded-full animate-spin"></div>
-    </div>
-  );
-}
-
-export default LoaderPage;
