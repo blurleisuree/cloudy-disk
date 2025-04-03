a7461d4945d271f4f3ba45090d98df6653b48e08
blurleisure <ipishir@gmail.com>
2025-04-03 22:38:33 +0300

    feat: add Router comp && routes conf && refactor App

--- client/src/App/App.jsx
+++ client/src/App/App.jsx
@@ -1,29 +1,12 @@
 import classes from "./App.module.css";
 
-import { BrowserRouter, Routes, Route, Navigate } from "react-router";
+import { BrowserRouter } from "react-router";
 import useAuthCheck from "../shared/hooks/useAuthCheck.js";
-
-import AuthPage from "../pages/AuthPage/AuthPage";
-import {
-  AuthContainer,
-  ResetPassForm,
-  ForgotForm,
-  VerifyForm,
-  AuthForm,
-  PublicRoute,
-  ProtectedRoute,
-} from "../modules/Auth/index.js";
-
-import Main from "../pages/Main/Main";
-import Files from "../pages/Files/Files";
-import Favourite from "../pages/Favourite/Favourite.jsx";
-import Profile from "../pages/Profile/Profile.jsx";
-import SettingsPage from "../pages/Profile/SettingsPage/SettingsPages.jsx";
-import DetailsPage from "../pages/Profile/DetailsPage/DetailsPage.jsx";
 
 import Message from "../shared/components/Message/Message.jsx";
 import Loader from "../shared/components/UI/Loader/Loader.jsx";
 import Modal from "../shared/components/Modal/Modal.jsx";
+import Router from "./components/Router/Router.jsx";
 
 function App() {
   const { loading } = useAuthCheck();
@@ -34,32 +17,7 @@ function App() {
     <div className={classes.App}>
       <Message />
       <Modal />
-      <Routes>
-        <Route element={<PublicRoute />}>
-          <Route element={<AuthPage />}>
-            <Route path="/auth" element={<AuthContainer />}>
-              <Route index element={<AuthForm />} />
-              <Route path="verify" element={<VerifyForm />} />
-              <Route path="forgot-password" element={<ForgotForm />} />
-              <Route path="reset-password" element={<ResetPassForm />} />
-            </Route>
-          </Route>
-        </Route>
-        <Route element={<ProtectedRoute />}>
-          <Route path="/" element={<Main />}>
-            <Route index element={<Navigate to="files" replace />} />
-            {/* {filesPages.map((page) => <Route path={page.path} element={<Files type={page.type}/>} />)} */}
-            <Route path="files" element={<Files />} />
-            <Route path="favourite" element={<Favourite />} />
-            <Route path="profile" element={<Profile />}>
-              <Route index element={<Navigate to="details" replace />} />
-              <Route path="settings" element={<SettingsPage />} />
-              <Route path="details" element={<DetailsPage />} />
-            </Route>
-          </Route>
-        </Route>
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
+      <Router />
     </div>
   );
 }
